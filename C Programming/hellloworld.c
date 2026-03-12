6d02d2409fa22a5881953548d0684284585ef132
David Rodrigues <119936741+davidrodriguex@users.noreply.github.com>
2026-03-12 11:01:04 -0300

    Learn C language

---
+++ C Programming/hellloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Inclui a biblioteca Standard Input-Output
+
+int main (){ // Declara a função principal
+    // Código a ser executado pela função principal.
+    printf("Ola, Neps Academy\n"); // função de impressão na tela
+    printf("Ola,\nNeps Academy"); // \n é parametro para quebra de linha
+}
